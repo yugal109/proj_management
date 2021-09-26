bd7d3eae8889304ba5cfdb5e6282c521084ff0ec
yugal <yugalkhati570@gmail.com>
2021-09-26 20:31:25 +0545

    added database

--- database.js
+++ database.js
@@ -1,10 +1,13 @@
 "use strict";
-const { Pool } = require("pg");
+var Pool = require("pg").Pool;
 require("dotenv").config();
-const connectionString = process.env.DATABASE_URL;
 try {
-    const pool = new Pool({
-        connectionString
+    var pool = new Pool({
+        user: process.env.USERNAME,
+        host: "localhost",
+        database: process.env.DATABASENAME,
+        password: process.env.PASSWORD,
+        port: 5432
     });
     console.log("Successfully connected to database");
     module.exports = pool;
