433ec1ba2567886e0d4e55825e2614308cbacaf9
yugal <yugalkhati570@gmail.com>
2021-09-26 21:19:18 +0545

    please work

--- database.js
+++ database.js
@@ -2,15 +2,15 @@
 var Pool = require("pg").Pool;
 require("dotenv").config();
 try {
-    var pool = new Pool({
+  var pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
 
-        connectionString:process.env.DATABASE_URL
-      
-    });
-    console.log("Successfully connected to database");
-    module.exports = pool;
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  console.log("Successfully connected to database");
+  module.exports = pool;
+} catch (error) {
+  console.log(error);
 }
-catch (error) {
-    console.log(error);
-}
-
