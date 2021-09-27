33581822a8590ccc5a10357e135c9789e07ee08e
yugal <yugalkhati570@gmail.com>
2021-09-27 19:23:03 +0545

    added redis

--- database.js
+++ database.js
@@ -1,16 +1,16 @@
 "use strict";
-var Pool = require("pg").Pool;
+const { Pool } = require("pg");
 require("dotenv").config();
 try {
-  var pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-  console.log("Successfully connected to database");
-  module.exports = pool;
-} catch (error) {
-  console.log(error);
+    const pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
+    console.log("Successfully connected to database");
+    module.exports = pool;
+}
+catch (error) {
+    console.log(error);
 }
