8fde1c5ca2e66a2a84bc3bb4872eeeaeb68b46b7
yugal <yugalkhati570@gmail.com>
2021-09-30 23:54:52 +0545

    added test cache

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -58,6 +58,17 @@ router.get("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () 
         console.log(error);
     }
 }));
+router.get("/nonchache/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const id = req.params.id;
+        const user = yield pool.query("SELECT id,username,email FROM Users WHERE id=$1 ", [id]);
+        client.set(`user-${user.rows[0].id}`, JSON.stringify(user.rows[0]));
+        res.send(user.rows[0]);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}));
 router.post("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { username, email, password } = req.body;
--- utility/redisConnection.js
+++ utility/redisConnection.js
@@ -3,13 +3,15 @@ const redis = require("redis");
 require("dotenv").config();
 try {
     const client = redis.createClient(process.env.REDIS_TLS_URL
-      ,{
+        ? process.env.REDIS_TLS_URL
+        : process.env.REDIS_URL, {
         tls: {
             rejectUnauthorized: false,
         },
     });
     module.exports = client;
 }
-catch (_a) {
+catch (error) {
     console.log("Couldnot connect to redis.");
+    console.log(error);
 }
